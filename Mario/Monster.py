d9316af1c617879dacf955b0914dd73167fbfc05
chd4433 <chdkhj4433@naver.com>
2021-10-15 23:09:51 +0900

    mario monster

---
+++ Mario/Monster.py
@@ -0,0 +1,38 @@
+
+
+class Gomba:
+    def __init__(self):
+        # self.image = load_image('')
+        self.x = 0
+        self.y = 0
+        self.frame = 0
+    def update(self):
+        pass
+
+
+class fish:
+    def __init__(self):
+        # self.image = load_image('')
+        self.x = 0
+        self.y = 0
+        self.frame = 0
+    def update(self):
+        pass
+
+class plant:
+    def __init__(self):
+        # self.image = load_image('')
+        self.x = 0
+        self.y = 0
+        self.frame = 0
+    def update(self):
+        pass
+
+class turtle:
+    def __init__(self):
+        # self.image = load_image('')
+        self.x = 0
+        self.y = 0
+        self.frame = 0
+    def update(self):
+        pass
